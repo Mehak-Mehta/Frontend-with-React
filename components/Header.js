a6d6f2d574865a5252dceabb5751881ff14c5e80
Mehak-Mehta <76558546+Mehak-Mehta@users.noreply.github.com>
2021-04-29 15:06:43 +0530

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+
+
+const Header = () => {
+    return (
+        <header className="head">
+            
+            <h1>
+                Welcome To Hunter x Hunter's Fan Web Page!
+            </h1>
+            <h2>
+                Here You Will Find Info On All the hunters in the anime!
+            </h2>
+            
+                    
+        </header>
+    )}
+
+
+export default Header
